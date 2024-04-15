2f211e0895f1139722d6209d079a15b387be16f9
Irina Illustrova <illustrova@gmail.com>
2024-04-15 16:07:14 +0300

    fix(button): make green

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 export const Button = (props: ButtonProps) => {
   const { children, ...rest } = props;
   return (
-    <button style={{ color: "red" }} {...rest}>
+    <button style={{ color: "green" }} {...rest}>
       {children}
     </button>
   );
